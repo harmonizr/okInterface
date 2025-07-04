c2846cbb1b219feb72618ff6033bde00c81c8606
hzr <15355109+user_hzr@user.noreply.gitee.com>
2025-07-04 02:36:01 +0000

    update qishui.js.
    Signed-off-by: hzr <15355109+user_hzr@user.noreply.gitee.com>

--- qishui.js
+++ qishui.js
@@ -5,8 +5,19 @@ var obj = JSON.parse(body);//JSON.parse()将json形式的body转变成对象处�
 //obj.playlists[0].owner.is_vip = true;
 //obj.playlists[1].owner.is_vip = true;
 
+obj.data.sections[0].asset_group.assets[2].amount_type="day";
 obj.data.sections[0].asset_group.assets[2].amount=20;
+
+obj.data.sections[1].task_group.tasks[0].assets[0].amount_type="day";
 obj.data.sections[1].task_group.tasks[0].assets[0].amount=20;
+// let countdown_text={
+//     "countdown": {
+//         "duration": 20,
+//         "suffix": "天",
+//         "unit": 2,
+//         "format": "d"
+//     }
+// }
 //obj.data.sections[1].task_group.tasks[0].view.subtitle.countdown_text.countdown.duration=20;
 
 body = JSON.stringify(obj);//重新打包回json字符串
