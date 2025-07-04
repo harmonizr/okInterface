0842a57f3bc20c8b2280282f8636b7278f93ec69
hzr <15355109+user_hzr@user.noreply.gitee.com>
2025-07-04 02:55:19 +0000

    update qishui.js.
    Signed-off-by: hzr <15355109+user_hzr@user.noreply.gitee.com>

--- qishui.js
+++ qishui.js
@@ -10,15 +10,15 @@ obj.data.sections[0].asset_group.assets[2].amount=20;
 
 obj.data.sections[1].task_group.tasks[0].assets[0].amount_type="day";
 obj.data.sections[1].task_group.tasks[0].assets[0].amount=20;
-// let countdown_text={
-//     "countdown": {
-//         "duration": 20,
-//         "suffix": "天",
-//         "unit": 2,
-//         "format": "d"
-//     }
-// }
-//obj.data.sections[1].task_group.tasks[0].view.subtitle.countdown_text.countdown.duration=20;
+let countdown_text={
+    "countdown": {
+        "duration": 20,
+        "suffix": "天",
+        "unit": 2,
+        "format": "d"
+    }
+}
+obj.data.sections[1].task_group.tasks[0].view.subtitle.countdown_text.countdown.duration=20;
 
 body = JSON.stringify(obj);//重新打包回json字符串
 $done({body});//结束修改
