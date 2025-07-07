38c2fa724ac708c4f8e33e1d843920e990ad9500
hzr <15355109+user_hzr@user.noreply.gitee.com>
2025-07-07 02:17:41 +0000

    update qishui_song.js.
    Signed-off-by: hzr <15355109+user_hzr@user.noreply.gitee.com>

--- qishui_song.js
+++ qishui_song.js
@@ -3,7 +3,7 @@ var obj = JSON.parse(body);//JSON.parse()将json形式的body转变成对象处�
 if(obj.track.artists){
     for(let i in artists){
         obj.track.artists[i].user_info.is_vip = true;
-        obj.track.artists[].user_info.vip_stage = "free";
+        obj.track.artists[i].user_info.vip_stage = "free";
     }
 }
 //obj.track.artists[0].user_info.is_vip = true;
