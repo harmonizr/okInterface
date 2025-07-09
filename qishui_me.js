4e3a2358ead2534fc41805cf1958f8b9e3a2f8ab
hzr <15355109+user_hzr@user.noreply.gitee.com>
2025-07-09 03:23:52 +0000

    update  qishui_me.js.
    Signed-off-by: hzr <15355109+user_hzr@user.noreply.gitee.com>

--- qishui_me.js
+++ qishui_me.js
@@ -2,7 +2,7 @@ var body = $request.body;//声明一个变量body并以响应消息体赋值
 var obj = JSON.parse(body);//JSON.parse()将json形式的body转变成对象处理
 obj.limited_free_param.rewind_pre_intercept_type="ad_vip_song_guide_new";
 obj.limited_free_param.expire_time=1752087576;
-obj.limited_free_param[limited_free]=false;
+obj.limited_free_param.limited_free=false;
 
 body = JSON.stringify(obj);//重新打包回json字符串
 $done({body});//结束修改
