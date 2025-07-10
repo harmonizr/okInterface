2e3a6ab3bfce9d0476e5ba200e438fe2cecd85bd
hzr <15355109+user_hzr@user.noreply.gitee.com>
2025-07-10 12:33:13 +0000

    update qishui_song.js.
    Signed-off-by: hzr <15355109+user_hzr@user.noreply.gitee.com>

--- qishui_song.js
+++ qishui_song.js
@@ -1,21 +1,21 @@
 var body = $response.body;//声明一个变量body并以响应消息体赋值
 var obj = JSON.parse(body);//JSON.parse()将json形式的body转变成对象处理
-if(obj.track.artists){
-    for(let i in obj.track.artists){
-        obj.track.artists[i].user_info.is_vip = true;
-        obj.track.artists[i].user_info.vip_stage = "free";
-    }
-}
-//obj.track.artists[0].user_info.is_vip = true;
+// if(obj.track.artists){
+//     for(let i in obj.track.artists){
+//         obj.track.artists[i].user_info.is_vip = true;
+//         obj.track.artists[i].user_info.vip_stage = "free";
+//     }
+// }
+// //obj.track.artists[0].user_info.is_vip = true;
 
-//obj.track.label_info.only_vip_download=true;
-obj.track.label_info.only_vip_playable = false;
+// //obj.track.label_info.only_vip_download=true;
+// obj.track.label_info.only_vip_playable = false;
 
-obj.track.preview.duration = obj.track.colors.duration;
-obj.track.preview.start = 0;
+// obj.track.preview.duration = obj.track.colors.duration;
+// obj.track.preview.start = 0;
 
-obj.track.audition_info.start_time_ms = 0;
-obj.track.audition_info.duration_ms= obj.track.colors.duration;
+// obj.track.audition_info.start_time_ms = 0;
+// obj.track.audition_info.duration_ms= obj.track.colors.duration;
 
 
 //音质
