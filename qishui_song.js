0519959dc239c4440a5e817381950311d7a3c41b
hzr <15355109+user_hzr@user.noreply.gitee.com>
2025-08-19 06:46:18 +0000

    update qishui_song.js.
    Signed-off-by: hzr <15355109+user_hzr@user.noreply.gitee.com>

--- qishui_song.js
+++ qishui_song.js
@@ -1,12 +1,12 @@
 var body = $response.body;//声明一个变量body并以响应消息体赋值
 var obj = JSON.parse(body);//JSON.parse()将json形式的body转变成对象处理
-if(obj.track.artists){
-    for(let i in obj.track.artists){
-        obj.track.artists[i].user_info.is_vip = true;
-        obj.track.artists[i].user_info.vip_stage = "free";
-    }
-}
-// //obj.track.artists[0].user_info.is_vip = true;
+// if(obj.track.artists){
+//     for(let i in obj.track.artists){
+//         obj.track.artists[i].user_info.is_vip = true;
+//         obj.track.artists[i].user_info.vip_stage = "free";
+//     }
+// }
+obj.track.artists[0].user_info.is_vip = true;
 
 // //obj.track.label_info.only_vip_download=true;
 // obj.track.label_info.only_vip_playable = false;
