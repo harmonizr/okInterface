1ac9caa02e3889e429003448a60ce469d8ca1ebb
hzr <15355109+user_hzr@user.noreply.gitee.com>
2025-08-19 06:44:23 +0000

    update qishui_song.js.
    Signed-off-by: hzr <15355109+user_hzr@user.noreply.gitee.com>

--- qishui_song.js
+++ qishui_song.js
@@ -1,11 +1,11 @@
 var body = $response.body;//声明一个变量body并以响应消息体赋值
 var obj = JSON.parse(body);//JSON.parse()将json形式的body转变成对象处理
-// if(obj.track.artists){
-//     for(let i in obj.track.artists){
-//         obj.track.artists[i].user_info.is_vip = true;
-//         obj.track.artists[i].user_info.vip_stage = "free";
-//     }
-// }
+if(obj.track.artists){
+    for(let i in obj.track.artists){
+        obj.track.artists[i].user_info.is_vip = true;
+        obj.track.artists[i].user_info.vip_stage = "free";
+    }
+}
 // //obj.track.artists[0].user_info.is_vip = true;
 
 // //obj.track.label_info.only_vip_download=true;
@@ -19,12 +19,12 @@ var obj = JSON.parse(body);//JSON.parse()将json形式的body转变成对象处�
 
 
 //音质
-obj.track.label_info.quality_map.lossless.play_detail.need_vip = false;
-obj.track.label_info.quality_map.medium.play_detail.need_vip = false;
-obj.track.label_info.quality_map.highest.play_detail.need_vip = false;
-obj.track.label_info.quality_map.higher.play_detail.need_vip = false;
-obj.track.label_info.quality_map.spatial.play_detail.need_vip = false;
-obj.track.label_info.quality_only_vip_can_play=[""];
+// obj.track.label_info.quality_map.lossless.play_detail.need_vip = false;
+// obj.track.label_info.quality_map.medium.play_detail.need_vip = false;
+// obj.track.label_info.quality_map.highest.play_detail.need_vip = false;
+// obj.track.label_info.quality_map.higher.play_detail.need_vip = false;
+// obj.track.label_info.quality_map.spatial.play_detail.need_vip = false;
+// obj.track.label_info.quality_only_vip_can_play=[""];
 
 body = JSON.stringify(obj);//重新打包回json字符串
 $done({body});//结束修改
