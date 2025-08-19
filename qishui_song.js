9f65670109748d3f25d7015f648ea7fbc9cec9a2
hzr <15355109+user_hzr@user.noreply.gitee.com>
2025-08-19 06:47:18 +0000

    update qishui_song.js.
    Signed-off-by: hzr <15355109+user_hzr@user.noreply.gitee.com>

--- qishui_song.js
+++ qishui_song.js
@@ -6,7 +6,7 @@ var obj = JSON.parse(body);//JSON.parse()将json形式的body转变成对象处�
 //         obj.track.artists[i].user_info.vip_stage = "free";
 //     }
 // }
-obj.track.artists[0].user_info.is_vip = true;
+//obj.track.artists[0].user_info.is_vip = true;
 
 // //obj.track.label_info.only_vip_download=true;
 // obj.track.label_info.only_vip_playable = false;
