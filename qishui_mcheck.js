a1be778c8e1baa47fa8b91c841b2ce0d2763fa54
hzr <15355109+user_hzr@user.noreply.gitee.com>
2025-11-24 10:28:12 +0000

    update qishui_mcheck.js.
    Signed-off-by: hzr <15355109+user_hzr@user.noreply.gitee.com>

--- qishui_mcheck.js
+++ qishui_mcheck.js
@@ -1,16 +1,27 @@
 var body = $response.body;//声明一个变量body并以响应消息体赋值
 var obj = JSON.parse(body);//JSON.parse()将json形式的body转变成对象处理
-if(obj.media_status){
-    for(let i in obj.media_status){
-        obj.media_status[i].label_info.quality_only_vip_can_play = [""];
-        obj.media_status[i].label_info.quality_map.lossless.play_detail.need_vip = false;    
-        obj.media_status[i].label_info.only_vip_playable = false;
-        let limited_free_info = obj.media_status[i].limited_free_info
-        if(limited_free_info){
-             limited_free_info.limited_free = false;
-        }
-    }
+if(obj.media_status[0].label_info != null){
+    obj.media_status[0].label_info.quality_only_vip_can_play = [""];
+    obj.media_status[0].label_info.quality_map.lossless.play_detail.need_vip = false;
+    obj.media_status[0].label_info.quality_map.highest.play_detail.need_vip = false;
+    obj.media_status[0].label_info.quality_map.hi_res.play_detail.need_vip = false;
+    obj.media_status[0].label_info.quality_map.spatial.play_detail.need_vip = false;
+
 }
+if(obj.media_status){
+ obj.media_status[0].limited_free_info.expire_time = 60000;
+}
+// if(obj.media_status){
+//     for(let i in obj.media_status){
+//         obj.media_status[i].label_info.quality_only_vip_can_play = [""];
+//         obj.media_status[i].label_info.quality_map.lossless.play_detail.need_vip = false;    
+//         obj.media_status[i].label_info.only_vip_playable = false;
+//         let limited_free_info = obj.media_status[i].limited_free_info
+//         if(limited_free_info){
+//              limited_free_info.limited_free = false;
+//         }
+//     }
+// }
 
 body = JSON.stringify(obj);//重新打包回json字符串
 $done({body});//结束修改
