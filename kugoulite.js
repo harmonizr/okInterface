861a58ec6fa3d32a5553283d0b1903c3fa542e0f
hzr <15355109+user_hzr@user.noreply.gitee.com>
2025-12-07 05:38:10 +0000

    update kugoulite.js.
    Signed-off-by: hzr <15355109+user_hzr@user.noreply.gitee.com>

--- kugoulite.js
+++ kugoulite.js
@@ -1,6 +1,6 @@
 var body = $response.body;//声明一个变量body并以响应消息体赋值
 var obj = JSON.parse(body);//JSON.parse()将json形式的body转变成对象处理
-obj.data.m_clearday="1767157635"
+obj.data.vip_end_time="1767157635"
 
 body = JSON.stringify(obj);//重新打包回json字符串
 $done({body});//结束修改
