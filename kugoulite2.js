dacdbdba7d44aebb33a6652c138d02a87235bbc9
hzr <15355109+user_hzr@user.noreply.gitee.com>
2025-12-08 02:25:57 +0000

    add kugoulite2.js.
    Signed-off-by: hzr <15355109+user_hzr@user.noreply.gitee.com>

---
+++ kugoulite2.js
@@ -0,0 +1,7 @@
+var body = $response.body;//声明一个变量body并以响应消息体赋值
+var obj = JSON.parse(body);//JSON.parse()将json形式的body转变成对象处理
+
+obj.data.busi_vip_list[0].vip_end_time = "2025-12-31 12:32:53"
+
+body = JSON.stringify(obj);//重新打包回json字符串
+$done({body});//结束修改
