6d916eca68f3040971bdf6233f000d6d3c9384c0
hzr <15355109+user_hzr@user.noreply.gitee.com>
2026-01-04 07:45:43 +0000

    update ymjz.js.
    Signed-off-by: hzr <15355109+user_hzr@user.noreply.gitee.com>

--- ymjz.js
+++ ymjz.js
@@ -2,7 +2,7 @@ var body = $response.body;//声明一个变量body并以响应消息体赋值
 
 var obj = JSON.parse(body);//JSON.parse()将json形式的body转变成对象处理
 
-obj.result.vipTime = 1;
+obj.result.vipTime = "2026-07-17";
 obj.result.vipType = 1;
 body = JSON.stringify(obj);//重新打包回json字符串
 $done({body});//结束修改
