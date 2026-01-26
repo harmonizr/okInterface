fa0a228eb9839ba09c6dc33d7add168a26a1bb41
hzr <15355109+user_hzr@user.noreply.gitee.com>
2026-01-26 10:02:42 +0000

    update yyyb.js.
    Signed-off-by: hzr <15355109+user_hzr@user.noreply.gitee.com>

--- yyyb.js
+++ yyyb.js
@@ -1,9 +1,9 @@
 var body = $response.body;//声明一个变量body并以响应消息体赋值
 var obj = JSON.parse(body);//JSON.parse()将json形式的body转变成对象处理
-JSON.datas.subverterVo.vipDeadline = 1830246630000;
-JSON.datas.subverterVo.fzVipDeadline = 1830246630000;
-JSON.datas.subverterVo.vipFlag = 2;
-JSON.datas.subverterVo.fzVipLevel = 2;
-JSON.datas.subverterVo.xxyyVipFlag = 2;
+obj.datas.subverterVo.vipDeadline = 1830246630000;
+obj.datas.subverterVo.fzVipDeadline = 1830246630000;
+obj.datas.subverterVo.vipFlag = 2;
+obj.datas.subverterVo.fzVipLevel = 2;
+obj.datas.subverterVo.xxyyVipFlag = 2;
 body = JSON.stringify(obj);//重新打包回json字符串
 $done({body});//结束修改
