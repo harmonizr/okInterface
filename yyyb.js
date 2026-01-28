6ede9d99715bca1432581589829c16b5866750a9
hzr <15355109+user_hzr@user.noreply.gitee.com>
2026-01-28 07:36:10 +0000

    update yyyb.js.
    Signed-off-by: hzr <15355109+user_hzr@user.noreply.gitee.com>

--- yyyb.js
+++ yyyb.js
@@ -1,13 +1,22 @@
 var body = $response.body;//声明一个变量body并以响应消息体赋值
 var obj = JSON.parse(body);//JSON.parse()将json形式的body转变成对象处理
-obj.datas.subverterVo.vipDeadline = 1830246630000;
-obj.datas.subverterVo.fzVipDeadline = 1830246630000;
-obj.datas.subverterVo.vipFlag = 1;
-// obj.datas.subverterVo.fzVipLevel = 1;
-// obj.datas.subverterVo.xxyyVipFlag = 1;
-// obj.datas.isAudit = 1;
-// obj.datas.subverterVo.identity = "VIP";
-// obj.datas.vipMonthTimeFlag = false;
+
+obj.datas.xxyyVipLevel = 1;
+
+obj.datas.dpVipDeadline = 1769498970000;
+obj.datas.vipDeadline = 1769498970000;
+obj.datas.flippedVipDeadline = 1769498970000;
+obj.datas.xxyyVipDeadline = 1769498970000;
+obj.datas.zcnVipDeadline = 1769498970000;
+obj.datas.ybVipDeadline = 1769498970000;
+
+obj.datas.zcnVipFlag = 1;
+obj.datas.xxyyVipFlag = 1;
+obj.datas.vipFlag = 1;
+obj.datas.foreverVipFlag = 1;
+obj.datas.flippedForeverVipFlag = 1;
+
+
 
 body = JSON.stringify(obj);//重新打包回json字符串
 $done({body});//结束修改
