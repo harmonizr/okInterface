79f57c2b587cc36dcced3e2592ec9c8f7a38138c
hzr <15355109+user_hzr@user.noreply.gitee.com>
2026-01-28 09:22:51 +0000

    update yyyb.js.
    Signed-off-by: hzr <15355109+user_hzr@user.noreply.gitee.com>

--- yyyb.js
+++ yyyb.js
@@ -1,8 +1,11 @@
 
 // var body = $response.body;//声明一个变量body并以响应消息体赋值
 // var obj = JSON.parse(body);//JSON.parse()将json形式的body转变成对象处理
- var body = {};
-  var obj = {};
+var body = {};
+var datas = {};
+var obj = {
+    datas:datas
+};
 obj.datas.xxyyVipLevel = 1;
 
 obj.datas.fzVipDeadline = 1769498970000;
