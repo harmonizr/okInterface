3882ec1582a7ca50d3075f3d62a0e5f98552671f
hzr <15355109+user_hzr@user.noreply.gitee.com>
2026-03-13 06:00:36 +0000

    update dokaVip.js.
    Signed-off-by: hzr <15355109+user_hzr@user.noreply.gitee.com>

--- dokaVip.js
+++ dokaVip.js
@@ -1,7 +1,7 @@
 var body = $response.body;//声明一个变量body并以响应消息体赋值
 var obj = JSON.parse(body);//JSON.parse()将json形式的body转变成对象处理
   
-obj.data.is_vip = 1;
+obj.data.is_vip = true;
 
 //obj.data.expire_time = "2030-02-21 21:54:36";
 obj.data.remaining_count = 999;
