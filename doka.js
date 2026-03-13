3f7c2c30552e0e911ed2c17465ee9391182ec194
hzr <15355109+user_hzr@user.noreply.gitee.com>
2026-03-13 05:47:49 +0000

    update doka.js.
    Signed-off-by: hzr <15355109+user_hzr@user.noreply.gitee.com>

--- doka.js
+++ doka.js
@@ -2,6 +2,11 @@ var body = $response.body;//声明一个变量body并以响应消息体赋值
 var obj = JSON.parse(body);//JSON.parse()将json形式的body转变成对象处理
   
 obj.data.is_vip = true;
+obj.data.product_id = "com.ydgn.dokacamera.yearly";
+obj.data.expire_date = "2030-02-21 21:54:36";
+obj.data.trial_period = true;
+obj.data.is_vip = true;
+
 // obj.data.expire_time = "2030-02-21 21:54:36";
 // obj.data.remaining_count = 999;
 body = JSON.stringify(obj);//重新打包回json字符串
