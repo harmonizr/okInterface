ceed42599eea006de04dbf419c58adaae47605ab
hzr <15355109+user_hzr@user.noreply.gitee.com>
2026-03-13 05:41:39 +0000

    update doka.js.
    Signed-off-by: hzr <15355109+user_hzr@user.noreply.gitee.com>

--- doka.js
+++ doka.js
@@ -2,7 +2,7 @@ var body = $response.body;//声明一个变量body并以响应消息体赋值
 var obj = JSON.parse(body);//JSON.parse()将json形式的body转变成对象处理
   
 obj.data.is_vip = true;
-obj.data.expire_time = "2027-3-13"
+obj.data.expire_time = "2030-02-21 21:54:36";
 obj.data.remaining_count = 999;
 body = JSON.stringify(obj);//重新打包回json字符串
 $done({body});//结束修改
