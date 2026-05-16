a26d9e40ab732ead1cca2a61bf0ffd99fc2e58f9
hzr <15355109+user_hzr@user.noreply.gitee.com>
2026-05-16 15:57:31 +0000

    update tjb.js.
    Signed-off-by: hzr <15355109+user_hzr@user.noreply.gitee.com>

--- tjb.js
+++ tjb.js
@@ -1,5 +1,5 @@
 //console.log( $request)
-var header = $request.headers;//声明一个变量header并以响应消息体赋值
+var headers = $request.headers;//声明一个变量header并以响应消息体赋值
 //var obj = JSON.parse(header);//JSON.parse()将json形式的header转变成对象处理
 //console.log(obj)
 
@@ -16,9 +16,9 @@ for(let i=0;i<36;i++){
 }
 
 // 三个参数直接定义好
-header["x-copygo-client-key"]  = uuid;
-header["x-copygo-display-name"]  = "v_" + uuid.split('-')[0];
-header["user-agent"] =  ["Mozilla/5.0 (iPhone; CPU iPhone OS 17_5 like Mac OS X) AppleWebKit/605.1.15 Mobile Safari/604.1","Mozilla/5.0 (Linux; Android 14; SM‑S9110) AppleWebKit/537.36 Chrome/129.0.0.0 Mobile","Mozilla/5.0 (Linux; Android 13; MI‑14) AppleWebKit/537.36 Chrome/128.0.0.0 Mobile"][Math.floor(Math.random()*3)];
+headers["x-copygo-client-key"]  = uuid;
+headers["x-copygo-display-name"]  = "v_" + uuid.split('-')[0];
+headers["user-agent"] =  ["Mozilla/5.0 (iPhone; CPU iPhone OS 17_5 like Mac OS X) AppleWebKit/605.1.15 Mobile Safari/604.1","Mozilla/5.0 (Linux; Android 14; SM‑S9110) AppleWebKit/537.36 Chrome/129.0.0.0 Mobile","Mozilla/5.0 (Linux; Android 13; MI‑14) AppleWebKit/537.36 Chrome/128.0.0.0 Mobile"][Math.floor(Math.random()*3)];
 //console.log(header)
-$done({headers:header});//结束修改
+$done({headers});//结束修改
 console.log($request)
