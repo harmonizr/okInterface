80672b27b7a670ac512728dbc3c0477eb3ed5201
hzr <15355109+user_hzr@user.noreply.gitee.com>
2026-05-19 07:37:36 +0000

    update tjb.js.
    Signed-off-by: hzr <15355109+user_hzr@user.noreply.gitee.com>

--- tjb.js
+++ tjb.js
@@ -35,7 +35,7 @@ try{
     console.log($argument)
     console.log($argument.isRandom==false);
     let tjbUrl = $argument.tjbUrl;
-    if($argument=="链接2"){
+    if($argument.isOne=="链接2"){
         tjbUrl = $argument.tjbUrl2;
     }
     //一般情况下两个链接都填上，用这个标志判断是否两个交替执行
