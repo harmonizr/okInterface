8fddbdcf7b8acb19ecaf3f2c4ca4b38bd04664ae
hzr <15355109+user_hzr@user.noreply.gitee.com>
2026-05-20 02:10:34 +0000

    update tjb.js.
    Signed-off-by: hzr <15355109+user_hzr@user.noreply.gitee.com>

--- tjb.js
+++ tjb.js
@@ -120,7 +120,7 @@
 
         const mainResp = await postRequest(params);
         const mainJson = JSON.parse(mainResp.body);
-        console.log("mainJson"+mainJson);
+        console.log("mainJson"+JSON.stringify(mainJson));
         if (mainJson.code !== 200) {
             throw new Error(`提交失败: ${mainJson.message}`);
         }
