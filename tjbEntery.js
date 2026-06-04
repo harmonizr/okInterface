bbb4e7348e18b6ed6a7267b954d121bdefc4deb5
hzr <15355109+user_hzr@user.noreply.gitee.com>
2026-06-04 13:25:47 +0000

    add tjbEntery.js.
    Signed-off-by: hzr <15355109+user_hzr@user.noreply.gitee.com>

---
+++ tjbEntery.js
@@ -0,0 +1,31 @@
+{
+  "message": "操作成功",
+  "data": {
+    "shortId": "xoo7V2Yb6xzm",
+    "entry": {
+      "createdAt": 1779682340563,
+      "extras": {
+        "extra_code": "HU293"
+      },
+      "usedCount": 0,
+      "rawText": "68₤rOdc5wZXeX2《 https://m.tb.cn/h.R3DeJ3R  HU293 最高赚188加抵金",
+      "markedInvalidCount": 0,
+      "titleDisplay": null,
+      "deeplinkRendered": {
+        "ios": "tbopen://m.taobao.com/tbopen/index.html?action=ali.open.nav&module=h5&h5Url=https%3A%2F%2Fm.tb.cn%2Fh.R3DeJ3R",
+        "h5": "https://m.tb.cn/h.R3DeJ3R",
+        "android": "tbopen://m.taobao.com/tbopen/index.html?action=ali.open.nav&module=h5&h5Url=https%3A%2F%2Fm.tb.cn%2Fh.R3DeJ3R"
+      },
+      "shortId": "xoo7V2Yb6xzm",
+      "expiresAt": 1779941540563,
+      "shareUrl": "https://m.tb.cn/h.R3DeJ3R",
+      "markedFullCount": 0,
+      "maxUseTimes": 1,
+      "coreToken": "R3DeJ3R",
+      "status": "active",
+      "publisherDisplayName": "v_9af0be2d"
+    },
+    "isRevived": true
+  },
+  "code": 200
+}
