c3f5cf40489d62d76f01c4c869385eacc22c8c3f
hzr <15355109+user_hzr@user.noreply.gitee.com>
2026-06-08 02:42:32 +0000

    update jdj.js.
    Signed-off-by: hzr <15355109+user_hzr@user.noreply.gitee.com>

--- jdj.js
+++ jdj.js
@@ -3,6 +3,6 @@ var body = $response.body;//声明一个变量body并以响应消息体赋值
 var obj = JSON.parse(body);//JSON.parse()将json形式的body转变成对象处理
 
 obj.data.userQuota.remaining = 7;
-obj.data,checkSubmit.compensationActive = true;
+obj.data.checkSubmit.compensationActive = true;
 body = JSON.stringify(obj);//重新打包回json字符串
 $done({body});//结束修改
