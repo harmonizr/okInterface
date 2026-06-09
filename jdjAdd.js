e9962fc55ecb5cce1ddf8fb904fb88be9f71b1f7
hzr <15355109+user_hzr@user.noreply.gitee.com>
2026-06-09 05:30:41 +0000

    update jdjAdd.js.
    Signed-off-by: hzr <15355109+user_hzr@user.noreply.gitee.com>

--- jdjAdd.js
+++ jdjAdd.js
@@ -1,8 +1,8 @@
 (async () => {
     try {
         let i = Number($persistentStore.read("i")) || 0;
-        const randomUA = "";
-        const suffix = "";
+        let randomUA = "";
+        let suffix = "";
 
         //大于三次重置i为0，换个uid,小于三次，从$persistentStore.read拿之前的
         if(i>=3){
