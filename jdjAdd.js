9668de680c63f5620a77dfe5e2bd693c56545930
hzr <15355109+user_hzr@user.noreply.gitee.com>
2026-06-09 04:39:52 +0000

    update jdjAdd.js.
    Signed-off-by: hzr <15355109+user_hzr@user.noreply.gitee.com>

--- jdjAdd.js
+++ jdjAdd.js
@@ -178,11 +178,13 @@
              
         const inviteResp = await postRequest(inviteParams);
          
-                      console.log("11222........"+inviteResp)
-
+                      console.log("11222........")
+                      console.log(stringify(inviteResp))
         const inviteJson = JSON.parse(inviteResp.body);
 
-              console.log("22222........"+inviteJson)
+        console.log("22222........")
+        console.log(inviteJson)
+
 
         if (inviteJson.code !== 200) {
             throw new Error(`提交失败: ${inviteJson.message}`);
