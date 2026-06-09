5799c51bc07a160021f1b1f2aa98b25d2aaa75c7
hzr <15355109+user_hzr@user.noreply.gitee.com>
2026-06-09 06:02:27 +0000

    update jdjAdd.js.
    Signed-off-by: hzr <15355109+user_hzr@user.noreply.gitee.com>

--- jdjAdd.js
+++ jdjAdd.js
@@ -3,7 +3,24 @@
         let i = Number($persistentStore.read("i")) || 0;
         let randomUA = "";
         let suffix = "";
-
+         // 封装 $httpClient.post 为 Promise
+        function postRequest(params) {
+            return new Promise((resolve, reject) => {
+                $httpClient.post(params, (err, resp, body) => {
+                    if (err) return reject(err);
+                    resolve({ resp, body });
+                });
+            });
+        }
+        function getRequest(params) {
+            return new Promise((resolve, reject) => {
+                $httpClient.get(params, (err, resp, body) => {
+                    if (err) return reject(err);
+                    resolve({ resp, body });
+                });
+            });
+        }
+     
         //大于设置的次数重置i为0，换个uid,小于设置的次数，从$persistentStore.read拿之前的
         if(i>=$argument.times){
             i=0;
@@ -26,32 +43,46 @@
                 suffix += chars[randomIdx];
             }
             suffix = "nid_"+suffix;
+
+            let url = "https://acbull.site/api/jdj/set-display-name";
+            let headers = {
+                'origin':'https://acbull.site',
+                'user-agent':randomUA,
+                'content-length':'20',
+                'referer':'https://acbull.site/jdj/',
+                'accept':'*/*',
+                'sec-fetch-dest':'empty',
+                'priority':'u=3, i',
+                'sec-fetch-mode':'cors',
+                'x-network-id':suffix,
+                'sec-fetch-site':'same-origin',
+                'content-type':'application/json',
+                'accept-encoding':'gzip, deflate, br, zstd',
+                'accept-language':'zh-CN,zh-Hans;q=0.9',
+            };
+
+            let body = {"displayName":$argument.name};
+            var params = {
+                url:url,
+                timeout:5000,
+                headers:headers,
+                alpn:'h2',
+                body:JSON.stringify(body),
+            };
+            const getNameResp = await getRequest(params);
+            const getNameJson = JSON.parse(getNameResp.body);
+            if (getNameResp.resp.status !== 200) {
+                throw new Error(`获取 name 失败: ${getNameJson.message}`);
+            }
+            console.log("请求name成功:"+getNameResp.body);
+
+
         }else{
             console.log("i!=0....")
             suffix = $persistentStore.read("suffix");
             randomUA = $persistentStore.read("randomUA");
 
         }
-        
-
-        // 封装 $httpClient.post 为 Promise
-        function postRequest(params) {
-            return new Promise((resolve, reject) => {
-                $httpClient.post(params, (err, resp, body) => {
-                    if (err) return reject(err);
-                    resolve({ resp, body });
-                });
-            });
-        }
-        function getRequest(params) {
-            return new Promise((resolve, reject) => {
-                $httpClient.get(params, (err, resp, body) => {
-                    if (err) return reject(err);
-                    resolve({ resp, body });
-                });
-            });
-        }
-     
 
         console.log("start init.....")
         let firstUrl = "https://acbull.site/api/jdj/init";
